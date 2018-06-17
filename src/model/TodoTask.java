3cfe0961a6a01b4a28dfd21a00efe3e97bc37737
Dominik <dom.ullmann@gmail.com>
2018-06-17 19:40:33 +0100

    Initial changes
    Added a new model class TodoTask and created a very basic GUI

---
+++ src/model/TodoTask.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class TodoTask {
+	private LocalDate dateDue;
+	private String description;
+	private boolean completed;
+	
+	
+	public TodoTask(String description, LocalDate date)
+	{
+		this.description = description;
+		this.dateDue = date;
+		this.completed = false;
+	}
+	
+	public LocalDate getDateDue() {
+		return dateDue;
+	}
+	public void setDateDue(LocalDate dateDue) {
+		this.dateDue = dateDue;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isCompleted() {
+		return completed;
+	}
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	
+	
+	
+}
